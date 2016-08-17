5d0d65c6f0167222f9127532a10862b06f696345
Andreas Hennings <andreas@dqxtech.net>
2016-08-17 06:37:59 +0200

    Add doc comments on methods in class me_node_creation_CrumbsMonoPlugin.

--- me_node_creation/lib/CrumbsMonoPlugin.php
+++ me_node_creation/lib/CrumbsMonoPlugin.php
@@ -3,10 +3,28 @@
 
 class me_node_creation_CrumbsMonoPlugin implements crumbs_MonoPlugin {
 
+  /**
+   * @param crumbs_InjectedAPI_describeMonoPlugin $api
+   *   Injected API object, with methods that allows the plugin to further
+   *   describe itself.
+   *
+   * @return void
+   */
   function describe($api) {
     $api->setTitle('Set breadcrumbs for placeholder pages.');
   }
 
+  /**
+   * Find candidates for the parent path.
+   *
+   * @param string $path
+   *   The path that we want to find a parent for.
+   * @param array $item
+   *   Item as returned from crumbs_get_router_item()
+   *
+   * @return string
+   *   Parent path candidate.
+   */
   function findParent($path, $item) {
     switch ($item['route']) {
       case 'node/add/%/mlid/%':
