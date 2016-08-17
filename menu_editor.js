d460df7de155c4348791d43c93a09f2bab1105a9
Andreas Hennings <andreas@dqxtech.net>
2016-08-17 08:30:11 +0200

    Fix line endings in menu_editor.js.

--- menu_editor.js
+++ menu_editor.js
@@ -2,31 +2,31 @@
   Drupal.behaviors.menuEditor = {
     attach: function (context, settings) {
       $('#menu-editor-overview-form #menu-overview', context).each(function(){
-        
+
         // table elements
         var table = $(this);
         var tbody = $('tbody', this);
-        
+
         // delete all checkbox
         (function(){
           var th_delete = $('thead th.delete-checkbox', table);
           var master_delete_checkbox = $('<input type="checkbox" class="master-delete-checkbox" />');
           th_delete.html('<span>&nbsp;'+th_delete.html()+'</span>');
           th_delete.prepend(master_delete_checkbox);
-          
+
           var onchange = function(){
             var checked_now = master_delete_checkbox.attr('checked');
             $('tbody td.delete-checkbox :checkbox', table).attr('checked', checked_now);
           };
-          
+
           master_delete_checkbox.change(onchange);
-          
+
           // IE does not trigger the change event..
           if ($.browser.msie) {
             master_delete_checkbox.click(onchange);
           }
         })();
-        
+
         // freeze width of first column (currently disabled)
         if (false) {
           var w = $('td.drag', this).width();
@@ -34,7 +34,7 @@
             $(this).css('width', w+'px');
           });
         }
-        
+
         // get reference css for textareas
         var ref_input = $('td.path-edit input', this);
         var h = ref_input.height();
@@ -44,12 +44,12 @@
           'padding-bottom': ref_input.css('padding-bottom')
         };
         var div_ref_height = ref_input.parent().height();
-        
+
         // adjust height of existing description textareas
         $('td.description textarea', this).css(ref_css);
         // this is necessary because of the vertical-align:middle
         $('td.description div.form-item', this).css('height', div_ref_height+'px');
-        
+
         // description column resizing
         $('td.description textarea', this).focus(function(){
           table.addClass('focus-description-column');
